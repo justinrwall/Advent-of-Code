68ba9e320cac7cca272b4cfa3ed94f7fc127cc96
Justin Wall <justin@justinrwall.com>
2024-12-07 15:27:29 -0600

    day 6 part 2 (terrible)

--- 2024/Day 6/Python/part1.py
+++ 2024/Day 6/Python/part1.py
@@ -1,46 +1,48 @@
 # check if position is in bounds
-def in_bounds(x, y, map):
-  if (x > -1 and x < len(map[0])) and (y > -1 and y < len(map)):
-    return True
-  return False
+def in_bounds(x, y, width, height):
+  return x in range(width) and y in range(height)
 
 # read input_data from file
 with open("../input.txt", "r") as file:
   input_data = [list(line.strip()) for line in file.readlines()]
 
+facility_map = input_data
+facility_width = len(facility_map[0])
+facility_height = len(facility_map)
+
 # identify coordinates of obstacles and the guard
 obstacles = []
-guardX = None
-guardY = None
-for i in range(len(input_data)):
-  for j in range(len(input_data[0])):
+guard_x = None
+guard_y = None
+for i in range(facility_height):
+  for j in range(facility_width):
     if input_data[i][j] == "#":
       obstacles.append([i, j])
     if input_data[i][j] == "^":
-      guardX = j
-      guardY = i
+      guard_x = j
+      guard_y = i
 
 # cycle of turn vectors
 directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 turn = 0
 
-visited = [[guardY, guardX]]  # list of visited coordinates
+visited = [[guard_y, guard_x]]  # list of visited coordinates
 
-currentX = guardX
-currentY = guardY
-while in_bounds(currentX, currentY, input_data):
+current_x = guard_x
+current_y = guard_y
+while in_bounds(current_x, current_y, facility_width, facility_height):
   # next position based on direction vector cycle
-  nextX = currentX + directions[turn % 4][1]
-  nextY = currentY + directions[turn % 4][0]
+  nextX = current_x + directions[turn % 4][1]
+  nextY = current_y + directions[turn % 4][0]
 
-  if in_bounds(nextX, nextY, input_data):
+  if in_bounds(nextX, nextY, facility_width, facility_height):
     if [nextY, nextX] in obstacles:
       turn = turn + 1  # turn right
     else:
-      currentX = nextX  # move to next
-      currentY = nextY
-      if [currentY, currentX] not in visited:
-        visited.append([currentY, currentX])  # record as visited
+      current_x = nextX  # move to next
+      current_y = nextY
+      if [current_y, current_x] not in visited:
+        visited.append([current_y, current_x])  # record as visited
   else:
     break
 
