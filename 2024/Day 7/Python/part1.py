8024e90fde7d9f94553f65bd2f6b5307243d02fa
Justin Wall <justin@justinrwall.com>
2024-12-07 19:25:28 -0600

    cleanup

--- 2024/Day 7/Python/part1.py
+++ 2024/Day 7/Python/part1.py
@@ -1,11 +1,10 @@
 # build list of all possible results
-def calculate(nums, results):
+def calculate(nums):
   if len(nums) == 1:
-    results.append(nums[0])
-    return
+    return nums
 
-  calculate([nums[0] + nums[1]] + nums[2:], results)  # add
-  calculate([nums[0] * nums[1]] + nums[2:], results)  # multiply
+  return (calculate([nums[0] + nums[1]] + nums[2:]) +  # add
+          calculate([nums[0] * nums[1]] + nums[2:]))   # multiply
 
 # read input_data from file
 with open("../input.txt", "r") as file:
@@ -18,8 +17,7 @@ for line in input_data:
   operands = list(map(int, right.split()))
 
   # get results of all possible operand/operator combinations
-  results = []
-  calculate(operands, results)
+  results = calculate(operands)
 
   # if one of the possible results is correct
   if result in results:
--- 2024/Day 7/Python/part2.py
+++ 2024/Day 7/Python/part2.py
@@ -1,12 +1,11 @@
 # build list of all possible results
-def calculate(nums, results):
+def calculate(nums):
   if len(nums) == 1:
-    results.append(nums[0])
-    return
+    return nums
 
-  calculate([nums[0] + nums[1]] + nums[2:], results)                 # add
-  calculate([nums[0] * nums[1]] + nums[2:], results)                 # multiply
-  calculate([int(str(nums[0]) + str(nums[1]))] + nums[2:], results)  # concatenate
+  return (calculate([nums[0] + nums[1]] + nums[2:]) +                # add
+          calculate([nums[0] * nums[1]] + nums[2:]) +                # multiply
+          calculate([int(str(nums[0]) + str(nums[1]))] + nums[2:]))  # concatenate
 
 # read input_data from file
 with open("../input.txt", "r") as file:
@@ -19,8 +18,7 @@ for line in input_data:
   operands = list(map(int, right.split()))
 
   # get results of all possible operand/operator combinations
-  results = []
-  calculate(operands, results)
+  results = calculate(operands)
 
   # if one of the possible results is correct
   if result in results:
