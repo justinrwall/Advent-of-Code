f42a45d57b84b5aebca06a99a9aab237fd29eb74
Justin Wall <11316122+justinrwall@users.noreply.github.com>
2024-12-14 12:37:04 -0600

    refactored 2024 day 14 part 2

--- 2024/Day 14/Python/part2.py
+++ 2024/Day 14/Python/part2.py
@@ -1,33 +1,6 @@
 from re import findall
 from PIL import Image
 
-# build group starting at (i, j)
-def get_group(grid, i, j, value):
-  group = [(i, j)]
-
-  # if cell outside grid bounds
-  if i not in range(len(grid)):
-    return []
-  if j not in range(len(grid[0])):
-    return []
-
-  # if cell value not right
-  if grid[i][j] != value:
-    return []
-
-  # if cell already in a group
-  if visited[i][j]:
-    return []
-
-  visited[i][j] = True  # exclude cell from future groups
-
-  group = group + get_group(grid, i + 1, j, value)  # cell below
-  group = group + get_group(grid, i, j + 1, value)  # cell to right
-  group = group + get_group(grid, i - 1, j, value)  # cell above
-  group = group + get_group(grid, i, j - 1, value)  # cell to left
-
-  return group
-
 # convert map to image
 def print_map(m):
   width = len(m[0])
@@ -77,20 +50,8 @@ while True:
     # record new robot position
     facility[p_y][p_x] += 1
 
-  # all cells unvisited at start
-  visited = [[False for j in range(width)] for i in range(height)]
-
-  # build list of groups
-  grid = [['#' if x != 0 else ' ' for x in row] for row in facility]
-  groups = []
-  for m in range(height):
-    for n in range(width):
-      if not visited[m][n] and grid[m][n] == '#':  # start of new group
-        groups.append(get_group(grid, m, n, '#'))
-
-  # if any group of size 100 or more exists, probably found the tree
-  group_sizes = [len(group) for group in groups]
-  if max(group_sizes) > 100:
+  # if all robots in unique positions, found the tree
+  if max([facility[y][x] for y in range(height) for x in range(width)]) == 1:
     break
 
   i += 1
